34715b831fcfbf1914656b1dba7b625d6c03a5ef
alekye <alekye@qq.com>
2016-08-29 13:53:54 +0800

    add desc

--- UIScrollViewCycleTest/UIScrollViewCycleTest/ViewController.h
+++ UIScrollViewCycleTest/UIScrollViewCycleTest/ViewController.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ *  UIScrollView 轮播的要点：
+    1，添加3个View，显示中间的一个
+    2，滚动结束后，根据contentOffset判断滚动方向，设置当前显示的索引
+    3，重新加载数据
+ */
+
 @interface ViewController : UIViewController
 
 
